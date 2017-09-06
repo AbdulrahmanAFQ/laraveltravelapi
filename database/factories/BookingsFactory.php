1334e2c0655ed60f9077ce3fd0eb792f9433055e
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-09-06 16:15:57 -0700

    add bookings factory

---
+++ database/factories/BookingsFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Passenger::class,
+    function (Faker\Generator $faker) {
+
+        return [
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'book_id' => function () {
+                return factory(App\Book::class)->create()->id;
+            }
+        ];
+    });
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Book::class, function (Faker\Generator $faker) {
+
+        return [
+            'type'           => $faker->randomElement(['One Way', 'Tow Ways']),
+            'depart_date'    => $faker->dateTimeBetween('now', '+3 days'),
+            'depart_time'    => $faker->time(),
+            'return_date'    => $faker->dateTimeBetween('+5 days', '+30 days'),
+            'return_time'    => $faker->time(),
+            'dep_city'       => $faker->city,
+            'des_city'       => $faker->city,
+            'class'          => $faker->randomElement(['Economy', 'Business', 'First class']),
+            'total_adults'   => rand(1, 3),
+            'total_children' => rand(0, 5),
+        ];
+    });
